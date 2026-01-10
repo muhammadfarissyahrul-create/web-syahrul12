a194c7e74cde07e156edb548f01829cafc2c35ff
muhammadfarissyahrul-create <muhammadfarissyahrul@gmail.com>
2026-01-10 09:48:14 +0700

    Add files via upload

--- admin.php
+++ admin.php
@@ -12,8 +12,9 @@ if (!isset($_SESSION['username'])) {
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Real Madrid Jurnal | Admin</title>
+    <title>Real Madrid Journal | Admin</title>
     <link rel="icon" href="img/logo.png" />
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <style>
@@ -31,7 +32,7 @@ if (!isset($_SESSION['username'])) {
     		<!-- nav begin -->
     <nav class="navbar navbar-expand-sm bg-body-tertiary sticky-top bg-danger-subtle">
     <div class="container">
-        <a class="navbar-brand" target="_blank" href=".">Real Madrid Jurnal</a>
+        <a class="navbar-brand" target="_blank" href=".">Real Madrid Journal</a>
         <button
         class="navbar-toggler"
         type="button"
